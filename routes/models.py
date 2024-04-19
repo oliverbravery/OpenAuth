91456c72fc225e9d1c9505731321ea518975ac12
oliverbravery <dev@oliverbravery.uk>
2024-04-19 16:32:32 +0100

    Added standard representation of a response.

--- routes/models.py
+++ routes/models.py
@@ -7,3 +7,27 @@ class ResponseStatus(Enum):
     """
     SUCCESS = "success"
     ERROR = "error"
+    
+class Response(BaseModel):
+    """
+    Response model extending Pydantic's BaseModel class.
+    Represents a response object with a status and message.
+    
+    Args:
+        status (ResponseStatus): The status of the response.
+        message (str): The message of the response.
+    """
+    status: ResponseStatus
+    message: str
+    
+    def dict(self, *args, **kwargs) -> dict:
+        """
+        Returns the dictionary representation of the Response object.
+
+        Returns:
+            dict: Dictionary representation of the Response object.
+        """
+        return {
+            "status": self.status.value,
+            "message": self.message
+        }
