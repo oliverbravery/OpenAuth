8bd94402b8e9f574a7dc99dce6c878ed004d7b51
oliverbravery <dev@oliverbravery.uk>
2024-04-19 11:34:11 +0100

    Changed import order to avoid circular import error in main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-from routes.account import account_router
 from database.db_manager import DBManager
 from dotenv import load_dotenv
 import os
@@ -8,10 +7,12 @@ load_dotenv(override=True)
 DB_CONNECTION_STRING: str = os.getenv("DB_CONNECTION_STRING")
 DB_NAME: str = os.getenv("DB_NAME")
 
-app: FastAPI = FastAPI()
-app.include_router(account_router.router)
-
 db_manager: DBManager = DBManager(connection_string=DB_CONNECTION_STRING, db_name=DB_NAME)
+
+app: FastAPI = FastAPI()
+
+from routes.account import account_router
+app.include_router(account_router.router)
 
 @app.get("/")
 async def root():
