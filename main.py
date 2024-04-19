0150774443df3fbce903eca6de7b5c05ea37b12f
oliverbravery <dev@oliverbravery.uk>
2024-04-19 11:18:10 +0100

    DBManager instantiated in main.py for all database interactions.

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 from routes import accounts
+from database.db_manager import DBManager
+from dotenv import load_dotenv
+import os
+
+load_dotenv(override=True)
+DB_CONNECTION_STRING: str = os.getenv("DB_CONNECTION_STRING")
+DB_NAME: str = os.getenv("DB_NAME")
 
 app: FastAPI = FastAPI()
 app.include_router(accounts.router)
+
+db_manager: DBManager = DBManager(connection_string=DB_CONNECTION_STRING, db_name=DB_NAME)
 
 @app.get("/")
 async def root():
