20c3755ea605d68ecd7af189747428fdeb0404e7
oliverbravery <dev@oliverbravery.uk>
2024-04-19 12:36:50 +0100

    Refactor database connection setup in main.py

--- main.py
+++ main.py
@@ -4,10 +4,33 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv(override=True)
-DB_CONNECTION_STRING: str = os.getenv("DB_CONNECTION_STRING")
-DB_NAME: str = os.getenv("DB_NAME")
+MONGO_PORT: int = os.getenv("MONGO_PORT")
+MONGO_HOST: str = os.getenv("MONGO_HOST")
+MONGO_ROOT_USERNAME: str = os.getenv("MONGO_INITDB_ROOT_USERNAME")
+MONGO_ROOT_PASSWORD: str = os.getenv("MONGO_INITDB_ROOT_PASSWORD")
+MONGO_DATABASE_NAME: str = os.getenv("DMONGO_DATABASE_NAME")
 
-db_manager: DBManager = DBManager(connection_string=DB_CONNECTION_STRING, db_name=DB_NAME)
+def get_connection_string(port: int, host: str, username: str, password: str) -> str:
+    """
+    Composes a connection string for a MongoDB database.
+
+    Args:
+        port (int): Port number for the MongoDB database.
+        host (str): Hostname for the MongoDB database.
+        username (str): Username for the MongoDB database.
+        password (str): Password for the MongoDB database.
+
+    Returns:
+        str: Composed connection string for the MongoDB database.
+    """
+    return f"mongodb://{username}:{password}@{host}:{port}"
+
+mongo_connection_string: str = get_connection_string(port=MONGO_PORT, 
+                                                     host=MONGO_HOST, 
+                                                     username=MONGO_ROOT_USERNAME, 
+                                                     password=MONGO_ROOT_PASSWORD)
+
+db_manager: DBManager = DBManager(connection_string=mongo_connection_string, db_name=MONGO_DATABASE_NAME)
 
 app: FastAPI = FastAPI()
 
