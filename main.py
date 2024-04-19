d3e9d5777a5397915eb7b54fe00b41a17e73355e
oliverbravery <dev@oliverbravery.uk>
2024-04-19 11:28:52 +0100

    Refactor account routes to use account_router in main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from routes import accounts
+from routes.account import account_router
 from database.db_manager import DBManager
 from dotenv import load_dotenv
 import os
@@ -9,7 +9,7 @@ DB_CONNECTION_STRING: str = os.getenv("DB_CONNECTION_STRING")
 DB_NAME: str = os.getenv("DB_NAME")
 
 app: FastAPI = FastAPI()
-app.include_router(accounts.router)
+app.include_router(account_router.router)
 
 db_manager: DBManager = DBManager(connection_string=DB_CONNECTION_STRING, db_name=DB_NAME)
 
