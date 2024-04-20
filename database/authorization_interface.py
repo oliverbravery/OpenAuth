c0df2b8d208ec41a5136c5433a7feced22ab814f
oliverbravery <dev@oliverbravery.uk>
2024-04-20 11:57:57 +0100

    Add add_authorization method to AuthorizationInterface

--- database/authorization_interface.py
+++ database/authorization_interface.py
@@ -1,6 +1,6 @@
 from database.db_generic_interface import DBGenericInterface
 from pymongo.database import Database
-from database.models import DBCollection
+from database.models import DBCollection, Authorization
 
 class AuthorizationInterface(DBGenericInterface):
     """
@@ -12,3 +12,12 @@ class AuthorizationInterface(DBGenericInterface):
         Initializes the AuthorizationInterface object, creating the authorization collection if it does not already exist.
         """
         super().__init__(database=database, db_collection=DBCollection.AUTHORIZATION.value)
+        
+    def add_authorization(self, authorization: Authorization) -> int:
+        """
+        Adds an authorization to the database.
+
+        Returns:
+            int: 0 if the authorization was added successfully, -1 otherwise.
+        """
+        return self.add_generic(object=authorization)
