d30e87c94ad9988d476ecd653b1ad48e90caf9ee
oliverbravery <dev@oliverbravery.uk>
2024-04-20 21:28:39 +0100

    Add ClientsInterface class for interacting with the clients collection in the database

---
+++ database/clients_interface.py
@@ -0,0 +1,14 @@
+from database.db_generic_interface import DBGenericInterface
+from pymongo.database import Database
+from database.models import DBCollection, Client
+
+class ClientsInterface(DBGenericInterface):
+    """
+    Class for interacting with the clients collection in the database.
+    Derived from the DBGenericInterface class.
+    """
+    def __init__(self, database: Database) -> None:
+        """
+        Initializes the ClientsInterface object, creating the clients collection if it does not already exist.
+        """
+        super().__init__(database=database, db_collection=DBCollection.CLIENTS.value)
