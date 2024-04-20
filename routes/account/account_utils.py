a3c8a7bc3b70cc1b5ea524746e10afbb6416ef31
oliverbravery <dev@oliverbravery.uk>
2024-04-20 12:04:07 +0100

    Add register_account_in_db_collections method to AccountUtils

--- routes/account/account_utils.py
+++ routes/account/account_utils.py
@@ -1,4 +1,5 @@
 from main import db_manager
+from database.models import Account, Authorization
 
 def check_user_exists(username: str) -> bool:
     """
@@ -11,3 +12,23 @@ def check_user_exists(username: str) -> bool:
         bool: True if the user exists, False otherwise.
     """
     return bool(db_manager.accounts_interface.get_account(username=username))
+
+def register_account_in_db_collections(new_account: Account) -> int:
+    """
+    Register a new account in the database collections.
+    Adds the new account to the accounts collection and the authorization collection.
+
+    Args:
+        new_account (Account): The account to be registered.
+
+    Returns:
+        int: 0 if the account was successfully registered, -1 otherwise.
+    """
+    response: int = db_manager.accounts_interface.add_account(account=new_account)
+    if response == -1: return -1
+    authorization_object: Authorization = Authorization(username=new_account.username)
+    response: int = db_manager.authorization_interface.add_authorization(authorization=authorization_object)
+    if response == -1: 
+        response: int = db_manager.accounts_interface.delete_account(username=new_account.username)
+        return -1
+    return 0
