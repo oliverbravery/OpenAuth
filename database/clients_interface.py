fb83501155523013236c9bcd2bee222e8d474dc7
oliverbravery <dev@oliverbravery.uk>
2024-04-20 21:31:15 +0100

    Add get_client method to ClientsInterface class

--- database/clients_interface.py
+++ database/clients_interface.py
@@ -12,3 +12,9 @@ class ClientsInterface(DBGenericInterface):
         Initializes the ClientsInterface object, creating the clients collection if it does not already exist.
         """
         super().__init__(database=database, db_collection=DBCollection.CLIENTS.value)
+        
+    def get_client(self, client_id: str) -> Client:
+        """
+        Gets the client with the specified client_id from the clients collection.
+        """
+        return self.get_generic(search_params={"client_id": client_id}, object_class=Client)
