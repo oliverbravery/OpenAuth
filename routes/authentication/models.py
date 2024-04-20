c3f34d9295e14354b3a1e9e0c84da18f3a6324fc
oliverbravery <dev@oliverbravery.uk>
2024-04-20 11:38:15 +0100

    Removed redundant LoginCredentialsForm in favour of AuthorizationForm

--- routes/authentication/models.py
+++ routes/authentication/models.py
@@ -1,18 +1,5 @@
 from fastapi.param_functions import Form
 from enum import Enum
-
-class LoginCredentialsForm:
-    """
-    A class used to represent the data required to authenticate a user. 
-    It is used to parse the data from the request body when validating a user.
-    """
-    def __init__(
-        self,
-        username: str = Form(),
-        password: str = Form(),
-    ):
-        self.username: str = username
-        self.password: str = password
         
 class AuthorizationForm:
     """
