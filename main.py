3b535edfb556667e87ec598c97dd41431e67716e
oliverbravery <dev@oliverbravery.uk>
2024-04-21 17:59:16 +0100

    Registered developer route with app router.

--- main.py
+++ main.py
@@ -40,6 +40,9 @@ app.include_router(account_router.router)
 from routes.authentication import authentication_router
 app.include_router(authentication_router.router)
 
+from routes.developer import developer_router
+app.include_router(developer_router.router)
+
 @app.get("/")
 async def root():
     return {"status": "Success", "message": "Welcome to the API!"}
