11efdb96a391cc64cd0cac24b4b14fbf0f5ea395
oliverbravery <dev@oliverbravery.uk>
2024-04-21 17:02:46 +0100

    Added BearerTokenAuth class for authenticating users using Bearer tokens.

--- routes/authentication/authentication_utils.py
+++ routes/authentication/authentication_utils.py
@@ -2,13 +2,15 @@ from main import db_manager
 from database.models import Account, Authorization, Client
 from routes.authentication.password_manager import PasswordManager
 from routes.authentication.token_manager import TokenManager
-from routes.authentication.models import TokenType, TokenResponse, RefreshToken
+from routes.authentication.models import TokenType, TokenResponse, RefreshToken, AccessToken
 from secrets import token_urlsafe
 import os
 from cryptography.fernet import Fernet
 from base64 import urlsafe_b64encode, urlsafe_b64decode
 import hashlib
 import bcrypt
+from fastapi import HTTPException, status
+from starlette.requests import Request
 
 fernet: Fernet = Fernet(os.getenv("AUTH_CODE_SECRET"))
 
@@ -239,3 +241,75 @@ def get_tokens_with_refresh_token(refresh_token: str) -> TokenResponse:
     user_account: Account = db_manager.accounts_interface.get_account(username=decoded_token.sub)
     if not user_account: return None
     return generate_and_store_tokens(authorization=authorization, user_account=user_account, client_id=decoded_token.aud[0])
+
+class BearerTokenAuth:
+    """
+    A class used to authenticate a user using a Bearer token.
+    """
+    token_prefix: str
+    
+    def __init__(self, token_prefix: str = "Bearer"):
+        """
+        The constructor for the BearerTokenAuth class.
+
+        Args:
+            token_prefix (str, optional): The prefix for the Bearer token. Defaults to "Bearer".
+        """
+        self.token_prefix = token_prefix
+        
+    def abstract_token_from_header(self, auth_header: str | None) -> str:
+        """
+        Abstracts the token from the Authorization header.
+
+        Args:
+            auth_header (str | None): The Authorization header as a string.
+
+        Returns:
+            str: The token as a string. None if the token is invalid or not present.
+        """
+        if not auth_header: return None
+        split_auth_header: list[str] = auth_header.split(" ")
+        if not len(split_auth_header) == 2: return None
+        if not split_auth_header[0] == self.token_prefix: return None
+        return split_auth_header[1]
+    
+    def raise_invalid_token_error(self) -> None:
+        """
+        Raises an HTTPException with status code 401 and a message indicating an invalid token.
+        """
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, 
+                            detail=f"Invalid {self.token_prefix} token")
+    
+    async def __call__(self, request: Request) -> Account:
+        """
+        Callable method to authenticate the user using the access Bearer token.
+        Use as an endpoint parameter to obtain the account of the user assosiated with the token.
+        Raises an HTTPException if the token is invalid.
+        
+        Example:
+        ```python
+        from fastapi import Depends, FastAPI
+        from routes.authentication.authentication_utils import BearerTokenAuth
+        app = FastAPI()
+        bearer_token_auth: BearerTokenAuth = BearerTokenAuth()
+        @app.get("/protected")
+        async def protected_endpoint(token: Account = Depends(bearer_token_auth)):
+            pass
+        ```
+
+        Args:
+            request (Request): Used to get the headers from the request.
+
+        Returns:
+            Account: The account object of the user associated with the token. Raises an HTTPException if the token is invalid.
+        """
+        auth_header = request.headers.get("Authorization")
+        token: str = self.abstract_token_from_header(auth_header=auth_header)
+        if not token: self.raise_invalid_token_error()
+        decoded_token: AccessToken = token_manager.verify_and_decode_jwt_token(token=token, token_type=TokenType.ACCESS)
+        if not decoded_token: self.raise_invalid_token_error()
+        account: Account = db_manager.accounts_interface.get_account(username=decoded_token.sub)
+        if not account: raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                                            detail="Issue fetching account information")
+        return account
+        
