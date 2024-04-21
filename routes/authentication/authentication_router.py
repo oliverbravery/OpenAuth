93d02d53c91335a22af65001ea2ddb85809ad43f
oliverbravery <dev@oliverbravery.uk>
2024-04-21 12:40:20 +0100

    Added /token endpoint for obtaining access tokens using either authentication code or refresh token.

--- routes/authentication/authentication_router.py
+++ routes/authentication/authentication_router.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, status, Depends, HTTPException
 from routes.authentication.authentication_utils import *
-from routes.authentication.models import AuthorizationForm, AuthorizeResponse
+from routes.authentication.models import AuthorizationForm, AuthorizeResponse, TokenForm, GrantType, TokenResponse
 from database.models import Authorization
 
 router = APIRouter(
@@ -26,3 +26,35 @@ async def register_account(form_data: AuthorizationForm = Depends()):
     if response == -1: raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
                             detail="Authorization failed.")
     return AuthorizeResponse(authorization_code=authorization_code, csrf_state=csrf_state).model_dump()
+
+@router.post("/token", status_code=status.HTTP_200_OK, response_model=TokenResponse)
+async def get_access_token(form_data: TokenForm = Depends()):
+    """
+    Get access token using the provided grant type.
+    Complies with OAuth2.0 Authorization Code Flow with Proof Key for Code Exchange (PKCE).
+
+    Args:
+        form_data (TokenForm): TokenForm object containing the OAuth2.0 /token request parameters.
+    """
+    token_response: TokenResponse = None
+    match form_data.grant_type:
+        case GrantType.AUTHORIZATION_CODE:
+            token_response = get_tokens_with_authorization_code(
+                auth_code=form_data.code,
+                code_verifier=form_data.code_verifier,
+                client_id=form_data.client_id,
+                client_secret=form_data.client_secret
+            )
+            if not token_response: raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                        detail="Invalid authorization code.")
+        case GrantType.REFRESH_TOKEN:
+            if form_data.refresh_token is None: raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                        detail="Refresh token is required for this grant type.")
+            token_response = get_tokens_with_refresh_token(
+                refresh_token=form_data.refresh_token)
+            if not token_response: raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                        detail="Invalid refresh token.")
+    if not token_response: raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                        detail="Token generation failed.")
+    return token_response.model_dump()
+            
