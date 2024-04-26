9ede7c218fe5abbc4a2542915bde6473e6be3f19
oliverbravery <dev@oliverbravery.uk>
2024-04-26 12:00:29 +0100

    Added generate_client_profile and create_profile_if_not_exists

--- services/account_services.py
+++ services/account_services.py
@@ -1,8 +1,10 @@
 from models.account_models import Account, AccountRole, Profile
 from common import db_manager
 from models.auth_models import Authorization
-from models.client_models import Client
+from models.client_models import Client, MetadataAttribute
 from models.util_models import ConcentDetails
+from utils.account_utils import generate_default_metadata
+from validators.user_validators import check_profile_exists
 
 def register_account_in_db_collections(new_account: Account) -> int:
     """
@@ -24,16 +26,45 @@ def register_account_in_db_collections(new_account: Account) -> int:
         return -1
     return 0
 
-def create_profile_if_not_exists(client_id: str, username: str) -> int:
+def generate_client_profile(client_id: str, scopes: str) -> Profile:
+    """
+    Generates a new profile based on client metadata and defaults.
+
+    Args:
+        client_id (str): The client id of the application.
+        scopes (str): The accepted scopes requested by the client.
+
+    Returns:
+        Profile: The new profile object.
+    """
     client: Client = db_manager.clients_interface.get_client(client_id=client_id)
-    if not client: return -1
-    # go through the client defaults and construct the client's profile based on them
+    if not client: return None
+    default_metadata: dict[str, any] = generate_default_metadata(profile_metadata_attributes=client.profile_metadata_attributes,
+                                                                 profile_defaults=client.profile_defaults)
+    scopes_as_list: list[str] = scopes.split(" ")
     new_profile: Profile = Profile(
         client_id=client_id,
-        role=client.get_profile_default(key="role"),
-        scopes=[],
-        metadata=
+        scopes=scopes_as_list,
+        metadata=default_metadata
         )
+    return new_profile
+
+def create_profile_if_not_exists(client_id: str, username: str, accecpted_scopes: str) -> int:
+    """
+    Creates a profile if it does not already exist.
+
+    Args:
+        client_id (str): Client id of the application.
+        username (str): The username of the user.
+        accecpted_scopes (str): The accepted scopes requested by the client.
+
+    Returns:
+        int: 0 if the profile was created successfully, -1 if the profile could not be created.
+    """
+    if check_profile_exists(username=username, client_id=client_id): return 0
+    new_profile: Profile = generate_client_profile(client_id=client_id, scopes=accecpted_scopes)
+    if not new_profile: return -1
+    return db_manager.accounts_interface.add_profile_to_account(username=username, profile=new_profile)
     
 def get_client_concent_details(client_id: str, scopes: list[str]) -> ConcentDetails:
     """
