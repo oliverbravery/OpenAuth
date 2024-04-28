68c41ecdb5c6a117aa37c472b6e36afc03da1dab
oliverbravery <dev@oliverbravery.uk>
2024-04-28 17:20:03 +0100

    Added check_client_scopes_have_unique_names and validate_client_scopes to scope_validators.py

--- validators/scope_validators.py
+++ validators/scope_validators.py
@@ -25,3 +25,35 @@ def valid_request_scopes(scopes: list[ProfileScope]) -> bool:
         matching_client_scopes: list[ClientScope] = [scope for scope in client.scopes if scope.name in str_scope_list and not scope.developer_only]
         if len(matching_client_scopes) != len(scope_list): return False
     return True
+
+def check_client_scopes_have_unique_names(scopes: list[ClientScope]) -> bool:
+    """
+    Check that the client's scopes have unique names.
+
+    Args:
+        scopes (list[ClientScope]): The scopes to check.
+
+    Returns:
+        bool: True if the client's scopes have unique names, False otherwise.
+    """
+    scope_names: list[str] = [scope.name for scope in scopes]
+    return len(set(scope_names)) == len(scope_names)
+
+def validate_client_scopes(client: Client) -> bool:
+    """
+    Validate that the client's scopes are unique and that the associated attributes exist in the client's metadata. 
+    
+    NOTE: If there are duplicate attributes for a scope this function will return False.
+
+    Args:
+        client (Client): The client to validate.
+
+    Returns:
+        bool: True if the client's scopes are valid, False otherwise.
+    """
+    if not check_client_scopes_have_unique_names(scopes=client.scopes): return False
+    metadata_attribute_names: list[str] = [metadata_attribute.name for metadata_attribute in client.profile_metadata_attributes]
+    for scope in client.scopes:
+        scope_attribute_names: list[str] = [scope_attribute.attribute_name for scope_attribute in scope.associated_attributes]
+        if len(scope_attribute_names) != len(set(scope_attribute_names)): return False
+        if not all([attribute in metadata_attribute_names for attribute in scope_attribute_names]): return False
