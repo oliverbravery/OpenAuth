c10d3b72e5c6ac6836d03181004b7dc4e30bb21c
oliverbravery <dev@oliverbravery.uk>
2024-04-28 11:57:43 +0100

    Refactor developer_router.py: Use verify_account_is_developer in enroll_developer function

--- routes/developer_router.py
+++ routes/developer_router.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter, status, Depends, HTTPException
 from models.account_models import Account, AccountRole
 from services.auth_services import BearerTokenAuth
 from services.account_services import enroll_account_as_developer
+from validators.account_validators import verify_account_is_developer
 
 router = APIRouter(
     prefix="/developer",
@@ -16,9 +17,7 @@ async def enroll_developer(account: Account = Depends(bearer_token_auth)):
     """
     Enroll the current account as a developer.
     """
-    if account.account_role == AccountRole.DEVELOPER:
-        raise HTTPException(status_code=status.HTTP_409_CONFLICT, 
-                            detail="Account is already enrolled as a developer.")
+    verify_account_is_developer(account=account)
     if enroll_account_as_developer(account) == -1:
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, 
                             detail="Failed to enroll account as a developer.")
