1f605701c71a4fc422d2418402782506fac23e22
oliverbravery <dev@oliverbravery.uk>
2024-04-28 16:13:21 +0100

    Added developer_only scope validation to valid_request_scopes

--- validators/scope_validators.py
+++ validators/scope_validators.py
@@ -1,11 +1,13 @@
 from models.client_models import Client
-from models.scope_models import ProfileScope
+from models.scope_models import ClientScope, ProfileScope
 from common import db_manager
 
 
 def valid_request_scopes(scopes: list[ProfileScope]) -> bool:
     """
     Check that the requested scopes are valid scopes that exist.
+    
+    NOTE: Scopes that are developer only are not allowed to be requested.
 
     Args:
         scopes (list[ProfileScope]): List of requested scopes to validate.
@@ -17,9 +19,9 @@ def valid_request_scopes(scopes: list[ProfileScope]) -> bool:
     for scope in scopes:
         client_to_scope[scope.client_id].append(scope)
     for client_id, scope_list in client_to_scope.items():
+        str_scope_list: list[str] = [scope.scope for scope in scope_list]
         client: Client = db_manager.clients_interface.get_client(client_id=client_id)
         if not client: return False
-        client_scope_names: list[str] = [scope.name for scope in client.scopes]
-        for requested_scope in scope_list:
-            if requested_scope.scope not in client_scope_names: return False
+        matching_client_scopes: list[ClientScope] = [scope for scope in client.scopes if scope.name in str_scope_list and not scope.developer_only]
+        if len(matching_client_scopes) != len(scope_list): return False
     return True
