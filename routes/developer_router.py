48ad326f785058a1ad53ac98a61bd9479ed45ec1
oliverbravery <dev@oliverbravery.uk>
2024-04-28 12:19:22 +0100

    Add base incomplete endpoint to add a new client in developer_router.py

--- routes/developer_router.py
+++ routes/developer_router.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, status, Depends, HTTPException
 
 from models.account_models import Account, AccountRole
+from models.form_models import ClientRegistrationForm
 from services.auth_services import BearerTokenAuth
 from services.account_services import enroll_account_as_developer
 from validators.account_validators import verify_account_is_developer
@@ -22,3 +23,15 @@ async def enroll_developer(account: Account = Depends(bearer_token_auth)):
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, 
                             detail="Failed to enroll account as a developer.")
     return "Account successfully enrolled as a developer."
+
+@router.post("/add-client", status_code=status.HTTP_200_OK)
+async def add_client(client_registration_form: ClientRegistrationForm, account: Account = Depends(bearer_token_auth)):
+    """
+    Add a new client to the database.
+    """
+    verify_account_is_developer(account=account)
+    # TODO: Validate the client and it's scopes
+    # TODO: Generate a client_id and client_secret
+    # TODO: Add the client to the database
+    # TODO: Return the client_id and client_secret
+    
