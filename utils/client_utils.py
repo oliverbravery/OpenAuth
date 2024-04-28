d9868d6ad82ca4d3f0bab8a9eda678b5825bc93d
oliverbravery <dev@oliverbravery.uk>
2024-04-28 17:28:21 +0100

    Add generate_client_credential function to client_utils.py

--- utils/client_utils.py
+++ utils/client_utils.py
@@ -1,5 +1,7 @@
 from models.client_models import Client
+import secrets
 
+from models.util_models import ClientCredentialType
 
 def get_profile_default_from_client(client: Client, key: str) -> any:
     """
@@ -12,3 +14,15 @@ def get_profile_default_from_client(client: Client, key: str) -> any:
         any: The default value for the given key. None if the key does not exist.
     """
     return client.profile_defaults.get(key, None)
+
+def generate_client_credential(credential_type: ClientCredentialType) -> str:
+    """
+    Generate a hex client credential for a client.
+
+    Args:
+        credential_type (ClientCredentialType): The type of credential to generate.
+
+    Returns:
+        str: The generated client credential.
+    """
+    return secrets.token_hex(credential_type.value)
