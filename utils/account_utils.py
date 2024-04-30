f6ef9f17fa3da681ebca63a72afcd2e0851bb0c5
oliverbravery <dev@oliverbravery.uk>
2024-04-30 17:57:04 +0100

    Add get_profile_attribute_from_account function to account_utils.py

--- utils/account_utils.py
+++ utils/account_utils.py
@@ -34,3 +34,19 @@ def get_profile_from_account(account: Account, client_id: str) -> Profile:
         if profile.client_id == client_id:
             return profile
     return None
+
+def get_profile_attribute_from_account(account: Account, client_id: str, attribute_name: str) -> any:
+    """
+    Get a specific attribute from a profile.
+
+    Args:
+        account (Account): The account to get the attribute from.
+        client_id (str): The client_id of the application the attribute belongs to.
+        attribute_name (str): The name of the attribute to get.
+
+    Returns:
+        any: The value of the attribute. None if the attribute does not exist.
+    """
+    profile: Profile = get_profile_from_account(account=account, client_id=client_id)
+    if not profile: return None
+    return profile.metadata.get(attribute_name)
