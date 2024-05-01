4c356da034b30a6377e04b5641fe96d16653d872
oliverbravery <dev@oliverbravery.uk>
2024-05-01 18:50:09 +0100

    Updated common.py to use config class to load environment variables

--- common.py
+++ common.py
@@ -1,66 +1,42 @@
 from fastapi import HTTPException, Request, status
 from fastapi.templating import Jinja2Templates
+from config.config import Config
 from database.db_manager import DBManager
 from cryptography.fernet import Fernet
 from models.account_models import Account
-from models.scope_models import ProfileScope
 from models.token_models import AccessToken, TokenType
 from models.util_models import AuthenticatedAccount
-from utils.scope_utils import str_to_list_of_profile_scopes
 from utils.token_manager import TokenManager
 from utils.database_utils import get_connection_string
-from dotenv import load_dotenv
-import os
 
-load_dotenv(override=True)
 
-MONGO_PORT: int = os.getenv("MONGO_PORT")
-MONGO_HOST: str = os.getenv("MONGO_HOST")
-MONGO_ROOT_USERNAME: str = os.getenv("MONGO_INITDB_ROOT_USERNAME")
-MONGO_ROOT_PASSWORD: str = os.getenv("MONGO_INITDB_ROOT_PASSWORD")
-MONGO_DATABASE_NAME: str = os.getenv("DMONGO_DATABASE_NAME")
-RECAPTCHA_SECRET_KEY: str = os.getenv("RECAPTCHA_SECRET_KEY")
-AUTH_CODE_SECRET: str = os.getenv("AUTH_CODE_SECRET")
-ACCESS_TOKEN_EXPIRE_MINUTES: str = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
-REFRESH_TOKEN_EXPIRE_MINUTES: str = os.getenv("REFRESH_TOKEN_EXPIRE_MINUTES")
-JWT_PRIVATE_PEM_PATH: str = os.getenv("JWT_PRIVATE_PEM_PATH")
-JWT_PUBLIC_PEM_PATH: str = os.getenv("JWT_PUBLIC_PEM_PATH")
-TOKEN_ALGORITHM: str = os.getenv("TOKEN_ALGORITHM")
-AUTH_CLIENT_ID: str = os.getenv("AUTH_CLIENT_ID")
-AUTH_CLIENT_SECRET: str = os.getenv("AUTH_CLIENT_SECRET")
-AUTH_SERVICE_HOST: str = os.getenv("AUTH_SERVICE_HOST")
-AUTH_SERVICE_PORT: str = os.getenv("AUTH_SERVICE_PORT")
-RECAPTCHA_SITE_KEY: str = os.getenv("RECAPTCHA_SITE_KEY")
+config: Config = Config()
 
-fernet: Fernet = Fernet(AUTH_CODE_SECRET)
+fernet: Fernet = Fernet(config.auth_config.authentication_code_secret)
 
-if not RECAPTCHA_SECRET_KEY: raise ValueError("RECAPTCHA_SECRET_KEY not set in environment variables.")
-google_verify_url: str = f"https://www.google.com/recaptcha/api/siteverify?secret={RECAPTCHA_SECRET_KEY}&response="
+google_verify_url: str = f"https://www.google.com/recaptcha/api/siteverify?secret={config.google_recaptcha_config.secret_key}&response="
 
 templates: Jinja2Templates = Jinja2Templates(directory="templates")
 
 token_manager: TokenManager = TokenManager(
-    access_token_expire_time=int(ACCESS_TOKEN_EXPIRE_MINUTES),
-    refresh_token_expire_time=int(REFRESH_TOKEN_EXPIRE_MINUTES),
-    private_key_path=str(JWT_PRIVATE_PEM_PATH),
-    public_key_path=str(JWT_PUBLIC_PEM_PATH),
-    token_algorithm=str(TOKEN_ALGORITHM)
+    access_token_expire_time=int(config.jwt_config.access_token_expire),
+    refresh_token_expire_time=int(config.jwt_config.refresh_token_expire),
+    private_key_path=str(config.jwt_config.private_key_path),
+    public_key_path=str(config.jwt_config.public_key_path),
+    token_algorithm=str(config.jwt_config.token_algorithm)
 )
 
 db_manager: DBManager = DBManager(
     connection_string=get_connection_string(
-        port=MONGO_PORT, 
-        host=MONGO_HOST, 
-        username=MONGO_ROOT_USERNAME, 
-        password=MONGO_ROOT_PASSWORD), 
-    db_name=MONGO_DATABASE_NAME)
+        port=config.database_config.port, 
+        host=config.database_config.host, 
+        username=config.database_config.username, 
+        password=config.database_config.password), 
+    db_name=config.database_config.name)
 
-db_manager.create_auth_service_client(
-    AUTH_CLIENT_ID=AUTH_CLIENT_ID, 
-    AUTH_CLIENT_SECRET=AUTH_CLIENT_SECRET, 
-    AUTH_SERVICE_HOST=AUTH_SERVICE_HOST, 
-    AUTH_SERVICE_PORT=AUTH_SERVICE_PORT
-)
+# Load the default client model into the database if it does not exist
+if db_manager.clients_interface.get_client(client_id=config.default_client_config.client_id) is None:
+    db_manager.clients_interface.add_client(client=config.load_client_model())
 
 class BearerTokenAuth:
     """
