5a3b2008897cc7b1526b41722865ed9b4371a90f
oliverbravery <dev@oliverbravery.uk>
2024-05-01 18:49:48 +0100

    load_client_model function validates all fields from the client model config json

--- config/config.py
+++ config/config.py
@@ -1,14 +1,16 @@
 from models.client_models import Client
-from models.config_models import AuthConfig, DatabaseConfig, DefaultClientConfig, GoogleRecaptchaConfig, JWTConfig
+from models.config_models import ApiConfig, AuthConfig, DatabaseConfig, DefaultClientConfig, GoogleRecaptchaConfig, JWTConfig
 from dotenv import load_dotenv
 from os import getenv
-
-load_dotenv(override=True)
+from utils.hash_utils import hash_string
+from validators.client_validators import validate_client_developers, validate_metadata_attributes, validate_profile_defaults
+from validators.scope_validators import validate_client_scopes
 
 class Config:
     """
     Used to load and store the environment variables.
     """
+    api_config: ApiConfig
     database_config: DatabaseConfig
     jwt_config: JWTConfig
     google_recaptcha_config: GoogleRecaptchaConfig
@@ -22,6 +24,11 @@ class Config:
         """
         Loads the environment variables.
         """
+        load_dotenv(override=True)
+        self.api_config = ApiConfig(
+            host=getenv("AUTH_HOST"),
+            port=int(getenv("AUTH_PORT"))
+        )
         self.database_config = DatabaseConfig(
             host=getenv("AUTH_MONGO_HOST"),
             port=int(getenv("AUTH_MONGO_PORT")),
@@ -53,6 +60,8 @@ class Config:
         """
         Loads the client model from the file path provided in the environment variables.
         
+        Validates the client model to ensure it is in the correct format and adds the client_id and client_secret from the environment variables.
+        
         Raises:
             ValueError: If the client model is not in the valid Client format.
 
@@ -62,6 +71,13 @@ class Config:
         with open(self.default_client_config.client_model_path, "r") as client_model_file:
             try:
                 default_client: Client = Client.model_validate_json(client_model_file.read())
+                default_client.client_id = self.default_client_config.client_id
+                default_client.client_secret = hash_string(plaintext=self.default_client_config.client_secret)
+                default_client.redirect_uri = f"http://{self.api_config.host}:{self.api_config.port}/account/login/callback"
+                if not validate_client_developers(client=default_client): raise ValueError("Client model does not have valid developers.")
+                if not validate_metadata_attributes(client=default_client): raise ValueError("Metadata attributes are not unique.")
+                if not validate_profile_defaults(client=default_client): raise ValueError("Profile defaults are not valid.")
+                if not validate_client_scopes(client=default_client): raise ValueError("Client model does not have valid scopes.")
                 return default_client
             except ValueError as e:
                 raise ValueError(f"Client model in file {self.default_client_config.client_model_path} is not in the valid Client format. {e}")
