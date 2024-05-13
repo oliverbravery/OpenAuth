386bab0d3a7f911de461b7c75b4e7102aa52b3b3
oliverbravery <dev@oliverbravery.uk>
2024-05-13 18:04:20 +0100

    Updated register form to accept the captcha response

--- models/form_models.py
+++ models/form_models.py
@@ -10,17 +10,11 @@ class UserRegistrationForm:
     A class used to represent a new account form. 
     It is used to parse the data from the request body when registering a new account.
     """
-    def __init__(
-        self,
-        username: str = Form(),
-        password: str = Form(),
-        email: str = Form(),
-        display_name: str = Form(),
-    ):
-        self.username: str = username
-        self.password: str = password
-        self.email: str = email
-        self.display_name: str = display_name
+    username: str = Form(),
+    password: str = Form(),
+    email: str = Form(),
+    display_name: str = Form(),
+    g_recaptcha_response: str = Form(alias="g-recaptcha-response")
         
 class LoginForm(AuthorizationRequest):
     """
