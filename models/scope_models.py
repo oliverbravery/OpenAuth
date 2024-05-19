b19a552c4f6326c3612a115c1b6968dc04938312
oliverbravery <dev@oliverbravery.uk>
2024-05-19 12:38:36 +0100

    Modified scopes of clients to allow for non-metadata attributes

--- models/scope_models.py
+++ models/scope_models.py
@@ -20,9 +20,7 @@ class ScopeAccessType(str, Enum):
     
 class ClientScopeAttribute(BaseModel):
     """
-    Represents an attribute of the client's specific metadata and what a scope can do with it.
-    
-    NOTE: This is only for the client's metadata attributes.
+    Represents an attribute in the client's metadata and what a scope can do with it.
     
     Args:
         attribute_name (str): The name of the attribute in the profile metadata.
@@ -30,6 +28,28 @@ class ClientScopeAttribute(BaseModel):
     """
     attribute_name: str
     access_type: ScopeAccessType
+    
+class AccountScopeAttribute(BaseModel):
+    """
+    Represents an attribute in the users account what a scope can do with it.
+    
+    Args:
+        attribute_name (AccountAttribute): The name of the attribute in the profile metadata.
+        access_type (ScopeAccessType): The access type of the attribute.
+    """
+    attribute_name: AccountAttribute
+    access_type: ScopeAccessType
+    
+class ScopeAssociatedAttributes(BaseModel):
+    """
+    Represents the associated attributes of a scope.
+    
+    Args:
+        client_attributes (list[ClientScopeAttribute]): The metadata attributes belonging to the clients profile.
+        account_attributes (list[AccountScopeAttribute]): The account attributes that the client can access from the user's account (not profile specific).
+    """
+    client_attributes: list[ClientScopeAttribute]
+    account_attributes: list[AccountScopeAttribute]
     
 class ClientScope(BaseModel):
     """
@@ -43,14 +63,14 @@ class ClientScope(BaseModel):
         shareable (bool): Whether other clients can request access to this scope.
         developer_only (bool): Whether the scope is only available to developers.
         is_personal_scope (bool): Whether the scope only allows the specific user to access the associated attributes or (if False) allow other linked users to access the associated attributes if they have the scope.
-        associated_attributes (List[ClientScopeAttribute]): The attributes (profile metadata) that this scope controls.
+        associated_attributes (ScopeAssociatedAttributes): The attributes that this scope controls.
     """
     name: str
     description: str
     shareable: bool
     developer_only: bool
     is_personal_scope: bool
-    associated_attributes: List[ClientScopeAttribute]
+    associated_attributes: ScopeAssociatedAttributes
 
 class ProfileScope(BaseModel):
     """
